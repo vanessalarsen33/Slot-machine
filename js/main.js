3c199eb5630f951c400b0415bc03e92de2f78fe1
Vanessa Larsen <vanessalarsen@vanessas-air.lan>
2020-05-11 10:45:09 -0600

    may11

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*----- constants -----*/
+const kawLookup = {
+   malala: {
+    img: 'images/malala.png';
+    }
+    kathrine: {
+        img:
+    }
+}
+
+/*----- app's state (variables) -----*/
+let coins, slotArray, results
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+document.querySelector('main button')
+  .addEventListener('click', playRound);
+
+
+
+/*----- functions -----*/
+
