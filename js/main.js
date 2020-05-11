96f76aad8f37684918d282c526d28818478f894c
Vanessa Larsen <vanessalarsen@vanessas-air.lan>
2020-05-11 16:18:26 -0600

    update0511/2nd

--- js/main.js
+++ js/main.js
@@ -6,31 +6,47 @@ kickassWomen = [
     },
     {
         name: "kathrine",
-        img: 'images/'
+        img: 'images/kathrine.png'
+    },
+    {
+        name: "kathrine",
+        img: 'images/kathrine.png'
+    },
+    {
+        name: "maya",
+        img: 'images/1.png'
     }
-]
-
+];
 /*----- app's state (variables) -----*/
-let tokens, slotArray, results
+let tokens, slotArray;
 
 /*----- cached element references -----*/
-
-
+// const slotEls = document.querySelectorAll('');
 
 /*----- event listeners -----*/
 document.querySelector('button').addEventListener('click', spin);
-
+$('#button').click(() => new Audio('sound/click.mp3').play() );
 
 
 /*----- functions -----*/
 
-function spin() {
-    var sOne = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
-    var sTwo = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
-    var sThree = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
+
+function spin(array) {
+    let sOne = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
+    let sTwo = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
+    let sThree = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
     console.log(sOne);
     console.log(sTwo);
     console.log(sThree);
+    console.log("slotid",document.getElementById('slot1'));
+
+
+    setTimeout(() => document.getElementById('slot1').style.backgroundImage = `url("${sOne.img}")`, 800);
+    setTimeout(() => document.getElementById('slot2').style.backgroundImage = `url("${sTwo.img}")`, 1500);
+    setTimeout(() => document.getElementById('slot3').style.backgroundImage = `url("${sThree.img}")`, 2000)
+
+    
 }
+
 
 
