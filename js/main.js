c49934f428b71284666025dacdf928962be6fb0d
Vanessa Larsen <vanessalarsen@vanessas-air.lan>
2020-05-13 09:03:11 -0600

    update0513

--- js/main.js
+++ js/main.js
@@ -2,51 +2,95 @@
 kickassWomen = [
     {
         name: "malala",
-        img: 'images/malala.png'
+        img: 'images/malala.png',
+        fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
     },
     {
         name: "kathrine",
-        img: 'images/kathrine.png'
+        img: 'images/kathrine.png',
+        fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
     },
     {
-        name: "kathrine",
-        img: 'images/kathrine.png'
+        name: "rosa",
+        img: 'images/rosa.png'
+        fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
     },
     {
         name: "maya",
-        img: 'images/1.png'
+        img: 'images/maya.png',
+        fact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
     }
 ];
 /*----- app's state (variables) -----*/
-let tokens, slotArray;
 
 /*----- cached element references -----*/
-// const slotEls = document.querySelectorAll('');
 
 /*----- event listeners -----*/
-document.querySelector('button').addEventListener('click', spin);
-$('#button').click(() => new Audio('sound/click.mp3').play() );
+document.querySelector('button').addEventListener('click', play);
 
 
 /*----- functions -----*/
 
 
-function spin(array) {
+function play() {
+    new Audio('sound/click.mp3').play()
     let sOne = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
     let sTwo = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
     let sThree = kickassWomen[Math.floor(Math.random() * kickassWomen.length)];
-    console.log(sOne);
-    console.log(sTwo);
-    console.log(sThree);
-    console.log("slotid",document.getElementById('slot1'));
+    console.log(sOne, sTwo, sThree, "first");
+    let slot1Idx = 0;
+    setTimeout(function(){
+        const playSlot1 = setInterval(function(){
+            const x =document.getElementById('slot1');
+            x.style.backgroundImage = `url("${kickassWomen[slot1Idx].img}")`
+            if(kickassWomen.length -1 === slot1Idx){
+                x.style.backgroundImage = `url("${sOne.img}")`
+                clearInterval(playSlot1)
+            } 
+            slot1Idx++
+        }, 700)
+    }, );
+    setTimeout(function(){
+            let slot2Idx = 1;
+            const playSlot2 = setInterval(function(){
+                const x =document.getElementById('slot2');
+            x.style.backgroundImage = `url("${kickassWomen[slot2Idx].img}")`
+            if(kickassWomen.length -1 === slot2Idx){
+                x.style.backgroundImage = `url("${sTwo.img}")`
+                clearInterval(playSlot2)
+            } 
+            slot2Idx++
+        }, 700)
+    }, 100)
+    setTimeout(function(){
+        let slot3Idx = 2;
+        const playSlot3 = setInterval(function(){
+            const x =document.getElementById('slot3');
+        x.style.backgroundImage = `url("${kickassWomen[slot3Idx].img}")`
+        if(kickassWomen.length -1 === slot3Idx){
+            x.style.backgroundImage = `url("${sThree.img}")`
+            clearInterval(playSlot3)
+        } 
+        slot3Idx++
+    }, 700)
+}, 200)
 
-
-    setTimeout(() => document.getElementById('slot1').style.backgroundImage = `url("${sOne.img}")`, 800);
-    setTimeout(() => document.getElementById('slot2').style.backgroundImage = `url("${sTwo.img}")`, 1500);
-    setTimeout(() => document.getElementById('slot3').style.backgroundImage = `url("${sThree.img}")`, 2000)
-
-    
+results(sOne, sTwo, sThree);
 }
 
+function results(a, b, c){
+    console.log(a.name, b.name, c.name);
+    if (a.name === b.name && b.name === c.name) {
+        console.log(1);
+        return true;
+    } else {
+        console.log(2);
+        return false;
+    }; 
+}
 
+function showResults(){
+    var result = $(results);
+    
+};
 
